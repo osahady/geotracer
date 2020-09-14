a0863eaf238f747f752a214ef720584f5f7af7e7
Osama Abdelhady <osahady@gmail.com>
2020-09-14 06:46:21 +0300

    centering position in screen

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -14,6 +16,15 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   final GeolocatorServiceAbuAli geoService = GeolocatorServiceAbuAli();
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  void initState() {
+    geoService.getCurrentLocation().listen((position) {
+      centerScreen(position);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +36,22 @@ class _MapState extends State<Map> {
         ),
         mapType: MapType.satellite,
         myLocationEnabled: true,
+        onMapCreated: (controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   } //build
+
+  Future<void> centerScreen(Position position) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 18.0,
+        ),
+      ),
+    );
+  }
 } //class
